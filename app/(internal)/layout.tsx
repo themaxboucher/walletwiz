8163d64fdf27c2dc1265b7fe7a9dc134a0b70226
Maxime Boucher <maxime@maximeboucher.com>
2025-05-25 19:44:17 -0600

    block auth pages for logged in users

--- app/(internal)/layout.tsx
+++ app/(internal)/layout.tsx
@@ -1,16 +1,10 @@
 import Logo from "@/components/Logo";
-import { getLoggedInUser } from "@/lib/actions/user.actions";
-import { redirect } from "next/navigation";
 
-export default async function AuthLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Block routes if user is logged in
-  const loggedIn = await getLoggedInUser();
-  if (loggedIn) redirect("/dashboard");
-
   return (
     <>
       <header className="w-full flex justify-center items-center px-8 py-6 absolute">
