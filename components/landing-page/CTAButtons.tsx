f63bfe32ab05e7affb810342892754ad287d798e
Maxime Boucher <maxime@maximeboucher.com>
2025-08-17 20:29:47 -0600

    fix CTA button bug

--- components/landing-page/CTAButtons.tsx
+++ components/landing-page/CTAButtons.tsx
@@ -7,12 +7,20 @@ export default function CTAButtons({ loggedIn }: { loggedIn: boolean }) {
       <div className="absolute inset-0 w-[40vw] h-20 rounded-full bg-primary/20 dark:bg-primary/10 blur-3xl left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
       {!loggedIn ? (
         <>
-          <Button asChild size="lg" className="sm:text-lg sm:py-6 sm:px-10">
+          <Button
+            asChild
+            size="lg"
+            className="relative z-10 sm:text-lg sm:py-6 sm:px-10"
+          >
             <Link href="/signup">Try WalletWiz — It's free</Link>
           </Button>
         </>
       ) : (
-        <Button asChild size="lg" className="sm:text-lg sm:py-6 sm:px-10">
+        <Button
+          asChild
+          size="lg"
+          className="relative z-10 sm:text-lg sm:py-6 sm:px-10"
+        >
           <Link href="/dashboard">View dashboard</Link>
         </Button>
       )}
