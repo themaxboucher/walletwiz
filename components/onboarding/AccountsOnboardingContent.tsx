5a8c8c03cd738251f043ea1dc3d117e6271ff16c
Maxime Boucher <maxime@maximeboucher.com>
2025-08-22 15:37:48 -0600

    polish onboarding

--- components/onboarding/AccountsOnboardingContent.tsx
+++ components/onboarding/AccountsOnboardingContent.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useState, useRef, useEffect, useLayoutEffect } from "react";
 import AccountsOnboardingForm from "./AccountsOnboardingForm";
 import CardStack from "../dashboard/CardStack";
+import { BlurFade } from "../magicui/blur-fade";
 
 interface AccountsOnboardingContentProps {
   user: User;
@@ -83,47 +84,55 @@ export default function AccountsOnboardingContent({
   return (
     <div className="w-full max-w-4xl space-y-10 min-h-[28rem] flex flex-col justify-between my-4">
       <div className="flex flex-col items-center text-center gap-1">
-        <h1 className="text-2xl font-medium">Add your financial accounts</h1>
-        <p className="text-sm text-muted-foreground mt-1 max-w-md">
-          Add at least one bank account or credit card to get started. You can
-          always add more later.
-        </p>
+        <BlurFade direction="up" className="space-y-2">
+          <h1 className="heading-3">Add your financial accounts</h1>
+          <p className="text-sm text-muted-foreground mt-1 max-w-md">
+            Add a bank account or credit card to get started.
+          </p>
+        </BlurFade>
+        <div className="flex items-center gap-1.5 mt-6">
+          <div className="h-1.5 w-8 bg-primary rounded-full" />
+          <div className="h-1.5 w-8 bg-primary/20 rounded-full" />
+          <div className="h-1.5 w-8 bg-primary/20 rounded-full" />
+        </div>
       </div>
 
-      <div className="grid grid-cols-1 items-center md:grid-cols-2 gap-10">
-        <AccountsOnboardingForm userId={user.$id} onAdd={handleAddAccount} />
-        {accounts.length == 0 && (
-          <div className="aspect-[1.75] w-full max-w-md border-2 border-dashed rounded-xl flex justify-center items-center">
-            <p className="text-sm text-muted-foreground">
-              Add your first account to get started.
-            </p>
-          </div>
-        )}
-        {accounts.length > 0 && (
-          <div
-            ref={scrollContainerRef}
-            className="relative h-full max-h-80 overflow-y-auto no-scrollbar mb-2"
-          >
+      <BlurFade direction="up" delay={0.2}>
+        <div className="grid grid-cols-1 items-center md:grid-cols-2 gap-10 mt-4">
+          <AccountsOnboardingForm userId={user.$id} onAdd={handleAddAccount} />
+          {accounts.length == 0 && (
+            <div className="aspect-[1.75] w-full max-w-md border-2 border-dashed rounded-xl flex justify-center items-center">
+              <p className="text-sm text-muted-foreground">
+                Add your first account to get started.
+              </p>
+            </div>
+          )}
+          {accounts.length > 0 && (
             <div
-              className={`sticky z-50 top-0 left-0 right-0 w-full h-12 bg-gradient-to-b from-background to-transparent pointer-events-none transition-opacity duration-300 ${
-                isAtTop ? "opacity-0" : "opacity-100"
-              }`}
-            />
-            <div className="px-6">
-              <CardStack
-                accounts={accounts}
-                className="-mb-12 -mt-9"
-                shadows={false}
+              ref={scrollContainerRef}
+              className="relative h-full max-h-80 overflow-y-auto no-scrollbar mb-2"
+            >
+              <div
+                className={`sticky z-50 top-0 left-0 right-0 w-full h-12 bg-gradient-to-b from-background to-transparent pointer-events-none transition-opacity duration-300 ${
+                  isAtTop ? "opacity-0" : "opacity-100"
+                }`}
+              />
+              <div className="px-6">
+                <CardStack
+                  accounts={accounts}
+                  className="-mb-12 -mt-9"
+                  shadows={false}
+                />
+              </div>
+              <div
+                className={`sticky z-50 bottom-0 left-0 right-0 w-full h-12 bg-gradient-to-t from-background to-transparent pointer-events-none transition-opacity duration-300 ${
+                  isAtBottom ? "opacity-0" : "opacity-100"
+                }`}
               />
             </div>
-            <div
-              className={`sticky z-50 bottom-0 left-0 right-0 w-full h-12 bg-gradient-to-t from-background to-transparent pointer-events-none transition-opacity duration-300 ${
-                isAtBottom ? "opacity-0" : "opacity-100"
-              }`}
-            />
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </BlurFade>
 
       <div className="flex justify-end gap-3 pt-2">
         {canContinue ? (
--- components/onboarding/CategoriesOnboardingContent.tsx
+++ components/onboarding/CategoriesOnboardingContent.tsx
@@ -8,11 +8,39 @@ import { useState } from "react";
 import { updateCategories } from "@/lib/actions/category.actions";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { BlurFade } from "../magicui/blur-fade";
 
 interface CategoriesOnboardingContentProps {
   user: User;
   categories: Category[];
 }
+
+const defaultSelectedCategories: Category[] = [
+  {
+    name: "Salary",
+    color: "blue",
+    iconName: "Briefcase",
+    type: "income",
+  },
+  {
+    name: "Housing",
+    color: "red",
+    iconName: "Home",
+    type: "expense",
+  },
+  {
+    name: "Utilities",
+    color: "yellow",
+    iconName: "Lightbulb",
+    type: "expense",
+  },
+  {
+    name: "Groceries",
+    color: "blue",
+    iconName: "ShoppingCart",
+    type: "expense",
+  },
+];
 
 export default function CategoriesOnboardingContent({
   user,
@@ -28,9 +56,9 @@ export default function CategoriesOnboardingContent({
   const selectedExpense = selectedCategories.filter(
     (c: Category) => c.type === "expense"
   );
-  // Check if at least 3 income and 3 expense categories are selected.
-  // Since 'Other' categories are selected by default, we need to check if there are at least 4 categories of each type.
-  const canContinue = selectedIncome.length >= 4 && selectedExpense.length >= 4;
+  // Check if at least 1 income and 3 expense categories are selected.
+  // Since 'Other' categories are selected by default, we need to check if there are at least 2 and 4 categories of each type.
+  const canContinue = selectedIncome.length >= 2 && selectedExpense.length >= 4;
 
   async function handleContinue() {
     if (!canContinue || loading) return;
@@ -50,18 +78,30 @@ export default function CategoriesOnboardingContent({
   return (
     <div className="w-full max-w-4xl space-y-8 min-h-[28rem] flex flex-col justify-between my-4">
       <div className="flex flex-col items-center text-center gap-1">
-        <h1 className="text-2xl font-medium">Select your categories</h1>
-        <p className="text-sm text-muted-foreground mt-1 max-w-md">
-          Choose at least 3 income and 3 expense categories to classify your
-          transactions. You can always change them and add more later.
-        </p>
+        <BlurFade direction="up" className="space-y-2">
+          <h1 className="heading-3">Select your categories</h1>
+          <p className="text-sm text-muted-foreground mt-1 max-w-md">
+            How do you want to categorize your transactions?
+          </p>
+        </BlurFade>
+        <div className="flex items-center gap-1.5 mt-6">
+          <div className="h-1.5 w-8 bg-primary/20 rounded-full" />
+          <div className="h-1.5 w-8 bg-primary rounded-full" />
+          <div className="h-1.5 w-8 bg-primary/20 rounded-full" />
+        </div>
       </div>
 
-      <CategoriesOnboardingForm
-        user={user}
-        onChange={setSelectedCategories}
-        selectedCategories={categories}
-      />
+      <BlurFade direction="up" delay={0.2}>
+        <CategoriesOnboardingForm
+          user={user}
+          onChange={setSelectedCategories}
+          selectedCategories={
+            categories.length > 3
+              ? categories
+              : [...categories, ...defaultSelectedCategories]
+          }
+        />
+      </BlurFade>
 
       <div className="flex justify-between items-start gap-3 pt-2">
         <Button asChild variant="outline">
@@ -72,7 +112,7 @@ export default function CategoriesOnboardingContent({
         <div className="flex flex-col items-end">
           <Button onClick={handleContinue} disabled={!canContinue || loading}>
             {loading ? (
-              <LoaderCircle className="h-4 w-4 animate-spin" />
+              <LoaderCircle className="size-4 animate-spin" />
             ) : (
               "Continue"
             )}
